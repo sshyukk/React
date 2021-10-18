04a694b752cd8baccb4ff4bcd377f569b67c631a
sshyukk <sanghyuk0328@gmail.com>
2021-10-18 18:18:36 +0900

    useMemo

--- VLPT/begin-react/src/App.js
+++ VLPT/begin-react/src/App.js
@@ -1,6 +1,11 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useMemo } from 'react'
 import UserList from './UserList'
 import CreateUser from './CreateUser'
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는 중...')
+  return users.filter(user => user.active === true).length
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -59,6 +64,7 @@ function App() {
       )
     )
   }
+  const count = useMemo(() => countActiveUsers(users), [users])
   return (
     <div>
       <div>
@@ -69,6 +75,7 @@ function App() {
           onCreate={onCreate} 
         />
         <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+        <div>활성 사용자 수: {count}</div>
       </div>
     </div>
   )
